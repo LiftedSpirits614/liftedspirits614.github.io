258ac25af812df7e928ee5e851d0f2204bfe0a16
LiftedSpirits614 <michael@getliftedspirits.com>
2025-03-12 20:46:14 +0000

    Update from https://github.com/LiftedSpirits614/GetLFTD/commit/06ab536ecbfe14b2c3985f66474444a1237fdbc3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "manifest.json": "aab3e69c285f52504f5a4473bdaf6ff8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "3ae0bec1a67a6eeb6f2911170df33b6c"};
+"flutter_bootstrap.js": "e9665131e2f0a96c2e4b0fefee30741b"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
