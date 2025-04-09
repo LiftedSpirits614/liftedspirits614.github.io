24a30f8a0eabcaaa12cc0cb2a00d0487dff36ce7
LiftedSpirits614 <michael@getliftedspirits.com>
2025-04-09 16:25:18 +0000

    Update from https://github.com/LiftedSpirits614/GetLFTD/commit/dc78bd4ad510c50891f0ef07c5b0e6f4d72c1a85

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2166444635"
+    serviceWorkerVersion: "4099224899"
   }
 });
